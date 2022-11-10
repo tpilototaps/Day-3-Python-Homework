a21499b94af7841bed4ec3d73331b925f8359234
tpilototaps <tpiloto@liberty.edu>
2022-11-10 09:35:10 -0500

    First Commit

---
+++ module_homework.py
@@ -0,0 +1,9 @@
+from math import pi
+
+def square_footage(length, width):
+    area = length * width
+    print(f"{area} ft^2")
+
+def circle_circumference(diameter):
+    circumference = pi * diameter
+    print(f'{circumference} ft')
